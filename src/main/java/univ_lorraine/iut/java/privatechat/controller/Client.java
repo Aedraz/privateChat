c70733fea06ad55019ee7d6489f41419f43a0e5b
T. Leucart <thomas.leucart3@etu.univ-lorraine.fr>
2023-03-30 09:13:55 +0200

    Ajout Server & Client

---
+++ src/main/java/univ_lorraine/iut/java/privatechat/controller/Client.java
@@ -0,0 +1,39 @@
+package univ_lorraine.iut.java.privatechat.controller;
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        String serverAddress = "";
+        int port = 12345;
+
+        Socket socket = new Socket(serverAddress, port);
+        System.out.println("Connexion établie avec le serveur.");
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+        String userInput;
+        while (true) {
+            System.out.print("Entrez un message (tapez 'quit' pour quitter) : ");
+            userInput = keyboard.readLine();
+
+            output.println(userInput);
+            if (userInput.equalsIgnoreCase("quit")) {
+                break;
+            }
+
+            String serverResponse = input.readLine();
+            System.out.println("Réponse du serveur: " + serverResponse);
+        }
+
+        System.out.println("Fermeture de la connexion.");
+        keyboard.close();
+        input.close();
+        output.close();
+        socket.close();
+    }
+}
---
+++ src/main/java/univ_lorraine/iut/java/privatechat/controller/Server.java
@@ -0,0 +1,34 @@
+package univ_lorraine.iut.java.privatechat.controller;
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        int port = 1234;
+        ServerSocket serverSocket = new ServerSocket(port);
+
+        System.out.println("Serveur en attente de connexion...");
+
+        Socket socket = serverSocket.accept();
+        System.out.println("Client connecté : " + socket.getInetAddress());
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+        String message;
+        while ((message = input.readLine()) != null) {
+            System.out.println("Message reçu: " + message);
+            output.println("Serveur: " + message);
+            if (message.equalsIgnoreCase("quit")) {
+                break;
+            }
+        }
+
+        System.out.println("Fermeture de la connexion.");
+        input.close();
+        output.close();
+        socket.close();
+        serverSocket.close();
+    }
+}
+
